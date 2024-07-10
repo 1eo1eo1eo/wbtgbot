34f382da02566a5690c9f60f791ce7986017f000
Lev <lafitel31@gmail.com>
2024-07-10 21:39:42 +1100

    in constants.py added parserfiles/ and mainfiles/ in paths

--- dockerfile
+++ dockerfile
@@ -9,15 +9,15 @@ RUN pip install --no-cache-dir -r requirements.txt
 RUN apt-get update && apt-get install -y default-mysql-client python3-pip \
     && playwright install && playwright install-deps
 
-RUN chmod +x  parserfiles.parser1/start_main.sh parserfiles.parser1/stop_main.sh \
-    parserfiles.parser2/start_main.sh parserfiles.parser2/stop_main.sh \
-    parserfiles.parser3/start_main.sh parserfiles.parser3/stop_main.sh \
-    parserfiles.parser4/start_main.sh parserfiles.parser4/stop_main.sh \
-    parserfiles.parser5/start_main.sh parserfiles.parser5/stop_main.sh \
-    parserfiles.parser6/start_main.sh parserfiles.parser6/stop_main.sh \
-    parserfiles.parser7/start_main.sh parserfiles.parser7/stop_main.sh \
-    parserfiles.parser8/start_main.sh parserfiles.parser8/stop_main.sh \
-    parserfiles.parser9/start_main.sh parserfiles.parser9/stop_main.sh \
-    parserfiles.parser10/start_main.sh parserfiles.parser10/stop_main.sh
+RUN chmod +x  parserfiles/parser1/start_main.sh parserfiles/parser1/stop_main.sh \
+    parserfiles/parser2/start_main.sh parserfiles/parser2/stop_main.sh \
+    parserfiles/parser3/start_main.sh parserfiles/parser3/stop_main.sh \
+    parserfiles/parser4/start_main.sh parserfiles/parser4/stop_main.sh \
+    parserfiles/parser5/start_main.sh parserfiles/parser5/stop_main.sh \
+    parserfiles/parser6/start_main.sh parserfiles/parser6/stop_main.sh \
+    parserfiles/parser7/start_main.sh parserfiles/parser7/stop_main.sh \
+    parserfiles/parser8/start_main.sh parserfiles/parser8/stop_main.sh \
+    parserfiles/parser9/start_main.sh parserfiles/parser9/stop_main.sh \
+    parserfiles/parser10/start_main.sh parserfiles/parser10/stop_main.sh
 
     CMD ["sh", "-c", "sleep 20 && python3 run.py"]
--- tgbotfiles/constants.py
+++ tgbotfiles/constants.py
@@ -13,58 +13,58 @@ CHAT_IDS = {"1111741813"} #, "525119691", "5162952042", "599786558", "5246704356
 
 PARSER_SCRIPTS = {
     1: {
-        "start": "/app/parser1/start_main.sh",
-        "stop": "/app/parser1/stop_main.sh"
+        "start": "/app/parserfiles/parser1/start_main.sh",
+        "stop": "/app/parserfiles/parser1/stop_main.sh"
     },
     2: {
-        "start": "/app/parser2/start_main.sh",
-        "stop": "/app/parser2/stop_main.sh"
+        "start": "/app/parserfiles/parser2/start_main.sh",
+        "stop": "/app/parserfiles/parser2/stop_main.sh"
     },
     3: {
-        "start": "/app/parser3/start_main.sh",
-        "stop": "/app/parser3/stop_main.sh"
+        "start": "/app/parserfiles/parser3/start_main.sh",
+        "stop": "/app/parserfiles/parser3/stop_main.sh"
     },
     4: {
-        "start": "/app/parser4/start_main.sh",
-        "stop": "/app/parser4/stop_main.sh"
+        "start": "/app/parserfiles/parser4/start_main.sh",
+        "stop": "/app/parserfiles/parser4/stop_main.sh"
     },
     5: {
-        "start": "/app/parser5/start_main.sh",
-        "stop": "/app/parser5/stop_main.sh"
+        "start": "/app/parserfiles/parser5/start_main.sh",
+        "stop": "/app/parserfiles/parser5/stop_main.sh"
     },
     6: {
-        "start": "/app/parser6/start_main.sh",
-        "stop": "/app/parser6/stop_main.sh"
+        "start": "/app/parserfiles/parser6/start_main.sh",
+        "stop": "/app/parserfiles/parser6/stop_main.sh"
     },
     7: {
-        "start": "/app/parser7/start_main.sh",
-        "stop": "/app/parser7/stop_main.sh"
+        "start": "/app/parserfiles/parser7/start_main.sh",
+        "stop": "/app/parserfiles/parser7/stop_main.sh"
     },
     8: {
-        "start": "/app/parser8/start_main.sh",
-        "stop": "/app/parser8/stop_main.sh"
+        "start": "/app/parser8/parserfiles/start_main.sh",
+        "stop": "/app/parser8/parserfiles/stop_main.sh"
     },
     9: {
-        "start": "/app/parser9/start_main.sh",
-        "stop": "/app/parser9/stop_main.sh"
+        "start": "/app/parser9/parserfiles/start_main.sh",
+        "stop": "/app/parser9/parserfiles/stop_main.sh"
     },
     10: {
-        "start": "/app/parser10/start_main.sh",
-        "stop": "/app/parser10/stop_main.sh"
+        "start": "/app/parser10/parserfiles/start_main.sh",
+        "stop": "/app/parser10/parserfiles/stop_main.sh"
     },
 }
 
 PID_FILE_PATHS = {
-    1: '/app/main_pid1.txt',
-    2: '/app/main_pid2.txt',
-    3: '/app/main_pid3.txt',
-    4: '/app/main_pid4.txt',
-    5: '/app/main_pid5.txt',
-    6: '/app/main_pid6.txt',
-    7: '/app/main_pid7.txt',
-    8: '/app/main_pid8.txt',
-    9: '/app/main_pid9.txt',
-    10: '/app/main_pid10.txt',
+    1: '/app/mainfiles/main_pid1.txt',
+    2: '/app/mainfiles/main_pid2.txt',
+    3: '/app/mainfiles/main_pid3.txt',
+    4: '/app/mainfiles/main_pid4.txt',
+    5: '/app/mainfiles/main_pid5.txt',
+    6: '/app/mainfiles/main_pid6.txt',
+    7: '/app/mainfiles/main_pid7.txt',
+    8: '/app/mainfiles/main_pid8.txt',
+    9: '/app/mainfiles/main_pid9.txt',
+    10: '/app/mainfiles/main_pid10.txt',
 }
 
 PRODUCT_TEMPLATE = """
